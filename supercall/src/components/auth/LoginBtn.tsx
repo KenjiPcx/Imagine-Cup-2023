f40f6d71ccafc92912aa925faf221ff56267fc73
Kenji Phang <ken.pcx@outlook.com>
2023-02-12 01:53:34 +0800

    Remove auth from buttons to fix deployment

--- supercall/src/components/auth/LoginBtn.tsx
+++ supercall/src/components/auth/LoginBtn.tsx
@@ -1,15 +1,9 @@
 import { Button } from "@hope-ui/solid";
-import { useMsal } from "./AuthProvider";
 import { loginRequest } from "../../scripts/authConfig";
 
 export default function LoginBtn() {
-  const { instance } = useMsal();
-  const handleLogin = () => {
-    instance.loginPopup(loginRequest);
-  };
-
   return (
-    <Button mr="$3" size="sm" onClick={handleLogin}>
+    <Button mr="$3" size="sm">
       Microsoft Login
     </Button>
   );
--- supercall/src/components/auth/LogoutBtn.tsx
+++ supercall/src/components/auth/LogoutBtn.tsx
@@ -1,14 +1,8 @@
 import { Button } from "@hope-ui/solid";
-import { useMsal } from "./AuthProvider";
 
 export default function LogoutBtn() {
-  const { instance } = useMsal();
-  const handleLogout = () => {
-    instance.logoutPopup();
-  };
-
   return (
-    <Button mr="$3" size="sm" onClick={handleLogout}>
+    <Button mr="$3" size="sm">
       Logout
     </Button>
   );
