17af9849dca8812c493c8d926b7782b642fdb485
Kenji Phang <ken.pcx@outlook.com>
2023-02-12 00:56:11 +0800

    Remove previous auth components

--- supercall/src/App.tsx
+++ supercall/src/App.tsx
@@ -4,18 +4,15 @@ import { initAuth } from "./scripts/authConfig";
 import { hopeConfig } from "./scripts/hopeConfig";
 import Layout from "./components/layout/Layout";
 import Pages from "./pages/Pages";
-import { AuthProvider } from "./components/auth/AuthProvider";
 
 const msalInstance = initAuth();
 
 const App: Component = () => {
   return (
     <HopeProvider config={hopeConfig}>
-      <AuthProvider instance={msalInstance}>
-        <Layout>
-          <Pages />
-        </Layout>
-      </AuthProvider>
+      <Layout>
+        <Pages />
+      </Layout>
     </HopeProvider>
   );
 };
